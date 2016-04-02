a52d5d509bf4d3cf3e3b48157f1bc0aebc658deb
Fat Deer <fatdeer@foxmail.com>
2016-04-02 12:45:09 +0800

    [240. Search a 2D Matrix II][Accepted]

---
+++ 240-Search-a-2D-Matrix-II/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+
+        len_m = len(matrix)
+        len_n = len(matrix[0])
+
+        i, j = 0, len_n - 1
+
+        while i < len_m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+
+        return False
+        
