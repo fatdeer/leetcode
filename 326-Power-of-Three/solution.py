d0775b6d7532f062fa0443bae6ff0cdd176ead52
Fat Deer <fatdeer@foxmail.com>
2016-04-12 01:59:27 +0800

    [326. Power of Three][Accepted]

--- 326-Power-of-Three/solution.py
+++ 326-Power-of-Three/solution.py
@@ -1,3 +1,11 @@
-bool isPowerOfThree(int n) {
-    return  n > 0 && 1162261467 % n == 0;
-}
+class Solution(object):
+    def __init__(self):
+        self.__max_pow3 = 3 ** int(math.log(0x7fffffff) / math.log(3))
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        #return n > 0 and 1162261467 % n == 0
+        
+        return n > 0 and self.__max_pow3 % n == 0
