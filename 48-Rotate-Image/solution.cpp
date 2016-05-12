ef6f5b3af40b13708ff9282cef698bfee84207f2
Fat Deer <fatdeer@foxmail.com>
2016-05-12 12:42:47 +0800

    [48. Rotate Image][Accepted]

---
+++ 48-Rotate-Image/solution.cpp
@@ -0,0 +1,7 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        return [list(reversed(x)) for x in zip(*matrix)]
