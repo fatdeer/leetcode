ed13d8dc34be13ed7d6920a9bedc82d2287b247a
Fat Deer <fatdeer@foxmail.com>
2016-06-01 19:06:34 +0800

    [74. Search a 2D Matrix][Accepted]

---
+++ 74-Search-a-2D-Matrix/solution.go
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty()) {
+            return false;
+        }
+
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        int left = 0;
+        int right = m * n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid / n][mid % n] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if (left != m * n && matrix[left / n][left % n] == target) {
+            return true;
+        }
+
+        return false;
+    }
+};
