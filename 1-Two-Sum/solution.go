ce6c821314b73fb9c42d353639bc4cc819081b6b
Fat Deer <fatdeer@foxmail.com>
2016-06-01 18:48:10 +0800

    [1. Two Sum][Accepted]

--- 1-Two-Sum/solution.go
+++ 1-Two-Sum/solution.go
@@ -1,31 +1,10 @@
 func twoSum(nums []int, target int) []int {
-    indexs := make([]int, 2)
-
-	hash := map[int]int{}
-
-	for i := range nums {
-		hash[target-nums[i]] = i
+	m := make(map[int]int)
+	for i, n := range nums {
+		if j, ok := m[n]; ok {
+			return []int{j, i}
+		}
+		m[target-nums[i]] = i
 	}
-
-	for i := range nums {
-		index, ok := hash[nums[i]]
-		if !ok {
-			continue
-		}
-
-		if index == i {
-		    continue
-		}
-		
-		if index > i {
-		    indexs[0] = i
-		    indexs[1] = index
-		} else {
-		    indexs[0] = index
-		    indexs[1] = i
-		}
-		
-		break
-	}
-	return indexs
+	return []int{0, 0}
 }
