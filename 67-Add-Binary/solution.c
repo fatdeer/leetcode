23f3d25c3f522e482fd5060b8eee5bed132a2fa9
Fat Deer <fatdeer@foxmail.com>
2016-06-05 23:09:39 +0800

    [67. Add Binary][Accepted]

---
+++ 67-Add-Binary/solution.c
@@ -0,0 +1,27 @@
+char* addBinary(char* a, char* b)
+{
+    int la = strlen(a);
+    int lb = strlen(b);
+    int lr = la > lb ? la : lb;
+    int carry = 0;
+    char* res = (char*)calloc(1, sizeof(char) * (lr + 2));  
+    res[lr + 1] = '\0';
+    la--;
+    lb--;
+
+    while (la >= 0 || lb >= 0) {
+        int ba = la >= 0 ? a[la--] - '0' : 0;
+        int bb = lb >= 0 ? b[lb--] - '0' : 0;
+        int br = ba ^ bb ^ carry;
+        carry = (ba & bb) | (carry & (ba ^ bb));
+        res[lr--] = br + '0';
+    }
+
+    if (!carry) {
+        return res + 1;
+
+    } else {
+        res[0] = '1';
+        return res;
+    }
+}
