5d676505f79aee16f3ceaae21b06b8f676550efc
Fat Deer <fatdeer@foxmail.com>
2016-06-07 00:00:16 +0800

    [62. Unique Paths][Accepted]

--- 62-Unique-Paths/solution.cpp
+++ 62-Unique-Paths/solution.cpp
@@ -1,15 +1,13 @@
-class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<int> f(n, 0);
-        f[0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                f[j] = f[j - 1] + f[j];
-            }
-        }
-
-        return f[n - 1];
-    }
-};
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [1] * n
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                dp[j] += dp[j-1]
+        return dp[-1]
+        
