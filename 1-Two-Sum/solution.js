c4e9cb26fda91fd791bf18f3044224fa0181f418
Fat Deer <fatdeer@foxmail.com>
2016-06-21 22:19:35 +0800

    [1. Two Sum][Accepted]

---
+++ 1-Two-Sum/solution.js
@@ -0,0 +1,10 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, n := range nums {
+		if j, ok := m[n]; ok {
+			return []int{j, i}
+		}
+		m[target-nums[i]] = i
+	}
+	return []int{0, 0}
+}
