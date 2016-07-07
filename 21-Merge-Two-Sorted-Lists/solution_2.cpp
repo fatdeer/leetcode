e1792dc95f923de04595a48619bd62cbb3774094
Fat Deer <fatdeer@foxmail.com>
2016-07-08 00:40:18 +0800

    [21. Merge Two Sorted Lists][Accepted]

---
+++ 21-Merge-Two-Sorted-Lists/solution_2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	var dummy = &ListNode{0, nil}
+	var current *ListNode
+	current = dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			current.Next = l1
+			l1 = l1.Next
+		} else {
+			current.Next = l2
+			l2 = l2.Next
+		}
+		current = current.Next
+	}
+
+	if l1 != nil {
+		current.Next = l1
+	} else {
+		current.Next = l2
+	}
+	return dummy.Next
+}
