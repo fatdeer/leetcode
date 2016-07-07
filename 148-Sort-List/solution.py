7404354becd4a2a57b50fdaae9330cfad60a8e05
Fat Deer <fatdeer@foxmail.com>
2016-07-08 00:12:57 +0800

    [148. Sort List][Accepted]

---
+++ 148-Sort-List/solution.py
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        auto slow = head, fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        fast = slow;
+        slow = slow->next;
+        fast->next = nullptr;
+
+        return mergeTwoLists(sortList(head), sortList(slow));
+    }
+
+private:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy{0};
+        auto curr = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+
+        return dummy.next;
+    }
+};
