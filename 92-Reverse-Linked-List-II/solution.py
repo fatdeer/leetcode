cb3f12c0c20cd5e8f880603bca032fc7f25dcb53
Fat Deer <fatdeer@foxmail.com>
2016-07-08 00:33:53 +0800

    [92. Reverse Linked List II][Accepted]

---
+++ 92-Reverse-Linked-List-II/solution.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        diff, dummy, cur = n - m + 1, ListNode(-1), head
+        dummy.next = head
+        
+        last_unswapped = dummy
+        while cur and m > 1:
+            cur, last_unswapped, m = cur.next, cur, m - 1
+            
+        prev, first_swapped = last_unswapped,  cur
+        while cur and diff > 0:
+            cur.next, prev, cur, diff = prev, cur, cur.next, diff - 1
+            
+        last_unswapped.next, first_swapped.next = prev, cur
+        
+        return dummy.next
