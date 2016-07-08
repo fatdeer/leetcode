b471c60eb577e7fcf88c1bed2d870f7e989ce87c
Fat Deer <fatdeer@foxmail.com>
2016-07-09 01:08:20 +0800

    [257. Binary Tree Paths][Accepted]

---
+++ 257-Binary-Tree-Paths/solution.cpp
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+        if not root:
+            return []
+        return [str(root.val) + '->' + path
+            for kid in (root.left, root.right) if kid
+            for path in self.binaryTreePaths(kid)] or [str(root.val)]
