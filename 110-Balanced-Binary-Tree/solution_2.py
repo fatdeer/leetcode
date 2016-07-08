4adb33fbf730b4600fbab86b81c96b8b22e34fa5
Fat Deer <fatdeer@foxmail.com>
2016-07-09 00:02:53 +0800

    [110. Balanced Binary Tree][Accepted]

---
+++ 110-Balanced-Binary-Tree/solution_2.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution(object):
+    def getHeight(self, root):
+        if root is None:
+            return 0
+        left_height, right_height = self.getHeight(root.left), self.getHeight(root.right)
+        if left_height < 0 or right_height < 0 or abs(left_height - right_height) > 1:
+            return -1
+        return max(left_height, right_height) + 1
+    
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        return self.getHeight(root) >= 0
+        
