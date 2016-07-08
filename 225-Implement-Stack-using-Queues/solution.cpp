e893781de4e9b227c560d15495719fc17d887193
Fat Deer <fatdeer@foxmail.com>
2016-07-08 21:23:33 +0800

    [225. Implement Stack using Queues][Compile Error]

---
+++ 225-Implement-Stack-using-Queues/solution.cpp
@@ -0,0 +1,31 @@
+class Stack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        
