70d00a4e7c0ec2beac351927c55afcdf8181899a
Fat Deer <fatdeer@foxmail.com>
2016-07-11 04:27:24 +0800

    [215. Kth Largest Element in an Array][Accepted]

---
+++ 215-Kth-Largest-Element-in-an-Array/solution.py
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int size = nums.size();
+        int left = 0, right = size - 1;
+        srand((unsigned)time(0));
+        for(int i = 0; i < size; ++i)
+            swap(nums[i], nums[rand()%size]);
+        while (true) {
+            int pos = partition(nums, left, right);
+            if (pos == k - 1)
+                return nums[pos];
+            if (pos > k - 1)
+                right = pos - 1;
+            else
+                left = pos + 1;
+        }
+    }
+    
+    int partition(vector<int>& nums, int left, int right) {
+        int pivot = nums[left];
+        int l = left + 1, r = right;
+        while (l <= r) {
+            if (nums[l] < pivot && nums[r] > pivot)
+                swap(nums[l++], nums[r--]);
+            if (nums[l] >= pivot)
+                l++;
+            if (nums[r] <= pivot)
+                r--;                                               
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
