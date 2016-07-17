57c36be403140899403174b4cb3280f0ea7338c8
Fat Deer <fatdeer@foxmail.com>
2016-07-17 11:37:53 +0800

    [119. Pascal's Triangle II][Accepted]

---
+++ 119-Pascal's-Triangle-II/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex + 1, 1);
+        
+        for (int i = 0; i < rowIndex + 1; ++i) {
+            for (int j = i - 1; j >= 1; --j) {
+                res[j] += res[j - 1];
+            }
+        }
+        return res;
+    }
+};
