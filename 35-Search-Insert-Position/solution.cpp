872573b3abc1d28e1364f52a925ea5aa774c32ca
Fat Deer <fatdeer@foxmail.com>
2016-07-20 00:13:45 +0800

    [35. Search Insert Position][Accepted]

--- 35-Search-Insert-Position/solution.cpp
+++ 35-Search-Insert-Position/solution.cpp
@@ -5,8 +5,9 @@ public:
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if(nums[mid] < target) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
