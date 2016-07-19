ece9a781b05754b3b5542bd85cb3d2dece8614f7
Fat Deer <fatdeer@foxmail.com>
2016-07-20 00:25:06 +0800

    [268. Missing Number][Accepted]

--- 268-Missing-Number/solution.cpp
+++ 268-Missing-Number/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int x = 0;
-        for (int i = 0; i <= nums.size(); i++) {
+        int x = nums.size();
+        for (int i = 0; i < nums.size(); i++) {
             x ^= i;
         }
         
