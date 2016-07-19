e25777411614d88708274b05db215c3619c7baa2
Fat Deer <fatdeer@foxmail.com>
2016-07-19 22:04:40 +0800

    [153. Find Minimum in Rotated Sorted Array][Compile Error]

--- 153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size();
         
+        if (size == 0) {
+            return 0;
+        } else if (size == 0) {
+            return nums[0];
+        } else if (size == 2) {
+            return min(nums[0], nums[1]);
+        }
+        
+        while (left < right) {
+            if (nums[left] <= nums[right - 1])  // ordered
+                return nums[left];
+            
+            // unordered
+            
+            int mid = left + (right - left)/2;
+            
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                if (mid == right - 1)
+                    return nums[mid];
+                else
+                    right = mid + 1;
+            }
+        }
+        return nums[left];
     }
 };
