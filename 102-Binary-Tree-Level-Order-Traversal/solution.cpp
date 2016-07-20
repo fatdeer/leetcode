33682157ac44bd5acbf3c95dea561f975c3822a3
Fat Deer <fatdeer@foxmail.com>
2016-07-20 16:07:22 +0800

    [102. Binary Tree Level Order Traversal][Accepted]

--- 102-Binary-Tree-Level-Order-Traversal/solution.cpp
+++ 102-Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -10,35 +10,35 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if (!root) {
-            return {};
+        int depth = getHeight(root);
+        vector<vector<int>> ret(depth);
+        
+        if (depth == 0) {
+            return ret;
         }
-
-        vector<int> row;
-        vector<vector<int>> result;
-        queue<TreeNode*> q;
-        q.push(root);
-        int count = 1;
-
-        while (!q.empty()) {
-            if (q.front()->left) {
-                q.push(q.front()->left);
-            }
-
-            if (q.front()->right) {
-                q.push(q.front()->right);
-            }
-
-            row.push_back(q.front()->val);
-            q.pop();
-
-            if (--count == 0) {
-                result.emplace_back(row);
-                row.clear();
-                count = q.size();
-            }
+        
+        getSolution(ret, root, 0);
+        return ret;
+    }
+    
+    void getSolution(vector<vector<int>>& ret, TreeNode* root, int level) {
+        if (root == NULL) {
+            return;
         }
-
-        return result;
+        
+        ret[level].push_back(root->val);
+        getSolution(ret, root->left, level + 1);
+        getSolution(ret, root->right, level + 1);
+    }
+    
+    int getHeight(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        
+        int left = getHeight(root->left);
+        int right = getHeight(root->right);
+        int height = ((left > right) ? left : right) + 1;
+        return height;
     }
 };
