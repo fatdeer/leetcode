ede7e96ad42af2457a1e4cbbcc6c2a8b450f98ac
Fat Deer <fatdeer@foxmail.com>
2016-07-25 16:10:08 +0800

    [70. Climbing Stairs][Accepted]

---
+++ 70-Climbing-Stairs/solution_2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int f1 = 2;
+        int f2 = 1;
+        if (n == 1) {
+            return f2;
+        } else if (n == 2) {
+            return f1;
+        }
+        
+        int fn;
+        for (int i = 3; i <= n; i++) {
+            fn = f1 + f2;
+            f2 = f1;
+            f1 = fn;
+        }
+        
+        return fn;
+    }
+};
