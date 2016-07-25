6fef55caed6275ad4852e7ed38f38279eb855f7d
Fat Deer <fatdeer@foxmail.com>
2016-07-25 23:08:48 +0800

    [90. Subsets II][Accepted]

---
+++ 90-Subsets-II/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        if (nums.empty()) {
+            return res;
+        }
+        
+        sort(nums.begin(), nums.end());
+        
+        res.push_back(vector<int>());
+        
+        vector<int> v;
+        
+        generate(0, v, nums);
+        
+        return res;
+    }
+    
+    void generate(int start, vector<int>& v, vector<int>& S) {
+        if (start == S.size()) {
+            return;
+        }
+        
+        for (int i = start; i < S.size(); i++) {
+            v.push_back(S[i]);
+            
+            res.push_back(v);
+            
+            generate(i + 1, v, S);
+            
+            v.pop_back();
+            
+            while (i < S.size() - 1 && S[i] == S[i + 1]) {
+                i++;
+            }
+        }
+    }
+};
